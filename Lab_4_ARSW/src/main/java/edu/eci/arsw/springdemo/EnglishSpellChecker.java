ada2f61f79a633c64c7b36da58e2812ccbb7d4d6
Esteban297 <estebancristancho9@gmail.com>
2022-09-07 22:44:45 -0500

    Lab 4 Finalizado

---
+++ Lab_4_ARSW/src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
@@ -0,0 +1,16 @@
+package edu.eci.arsw.springdemo;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+
+
+@Service("englishSpellChecker")
+public class EnglishSpellChecker implements SpellChecker {
+
+	@Override
+	public String checkSpell(String text) {
+		return "Checked with english checker: "+text;
+	}
+
+
+}
